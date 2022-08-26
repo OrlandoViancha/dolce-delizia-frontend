1f6e6a51f104600f0ae59e2408170e92012e935d
orviant20011904 <92343935+orviant20011904@users.noreply.github.com>
2022-08-26 16:35:46 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Menu from './Cliente/components/Menu/menu.component'
+import './App.scss';
+import './Cliente/pages/Login/login.css';
+import PublicRoutes from './Routes/routes.public'
+import PrivateRoutes from './Routes/routes.private'
+import {BrowserRouter as Router} from 'react-router-dom';
+import Footer from "./Cliente/components/footer/footer.component";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import isAuth from './controllers/logout'
+import Sidebar from './Administrador/components/sidebar/sidebar.js'
+import {DataProvider} from './controllers/context.js'
 function App() {
   return (
-    <div className="App">
+
+    
+  <Router>
+
+    <DataProvider>
+    
+    <div className="App"$>
+      
+    
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>hola mundo</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+
+          (localStorage.getItem("type_user")=="2" || localStorage.getItem("type_user")=="3")?
+          <Sidebar/>
+          
+          :
+          <Menu/>
+
+        }
+
+        
+       
+        
       </header>
+
+      <section className="App-main">
+
+      {
+           (isAuth()==true||localStorage.getItem("type_user")==null || localStorage.getItem("type_user")=="1")?
+
+           <PublicRoutes/>
+           :
+           <PrivateRoutes/>
+
+
+        }
+      </section>
+
+      
+      <Footer></Footer>
+      
     </div>
+
+    
+    </DataProvider> 
+  </Router>
+
+  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Axios from 'axios'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+Axios.defaults.baseURL = 'https://backend-dolcedelizia.herokuapp.com/'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +19,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
